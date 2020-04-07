033e768948dea5474086f4d02243a882964169fb
ccf23 <60208795+ccf23@users.noreply.github.com>
2020-04-07 14:06:43 -0400

    separated conditions for bloated and non-bloated

--- iteratenums.py
+++ iteratenums.py
@@ -11,31 +11,43 @@ def getConfigSpace():
     crash_grid = np.zeros((300,600))
 
     #variable label: object_number=[lowerx,lowery,upperx,uppery
-    r=0#in radius of bot to bloat obstacles
-    ob1=np.array([-4-r,4-r,-2+r,6+r])
-    ob2p1=np.array([-1-r,1-r,0+r,3+r])
-    ob2p2=np.array([0-r,2-r,1+r,3+r])
-    ob3=np.array([1-r,5-r,3+r,7+r])
+    
+    ob1=np.array([-4,4,-2,6])
+    ob2p1=np.array([-1,1,0,3])
+    ob2p2=np.array([0,2,1,3])
+    ob3=np.array([1,5,3,7])
+
+    #bloated obstacles to make crash_grid
+    r=0.49#in radius of bot to bloat obstacles
+    b_ob1=np.array([-4-r,4-r,-2+r,6+r])
+    b_ob2p1=np.array([-1-r,1-r,0+r,3+r])
+    b_ob2p2=np.array([0-r,2-r,1+r,3+r])
+    b_ob3=np.array([1-r,5-r,3+r,7+r])
     crash=np.array([[0,0,0,0]])
-    #count=0
+    
     i = 0
+    j = 0
     for lower_angle in lower_theta:
-        j = -300
+        j = 0
         for upper_angle in upper_theta:
             end_angle=lower_angle+upper_angle
             x=lower_length*np.cos(lower_angle)+upper_length*np.cos(end_angle)
             y=lower_length*np.sin(lower_angle)+upper_length*np.sin(end_angle)
-
-            if (x>=ob1[0] and x<=ob1[2] and y>=ob1[1] and y<=ob1[3] or
-            x>=ob2p1[0] and x<=ob2p1[2] and y>=ob2p1[1] and y<=ob2p1[3] or
-            x>=ob2p2[0] and x<=ob2p2[2] and y>=ob2p2[1] and y<=ob2p2[3] or
-            x>=ob3[0] and x<=ob3[2] and y>=ob3[1] and y<=ob3[3]):
-                crash=np.append(crash,np.array([[180/np.pi*(lower_angle),180/np.pi*(upper_angle),x,y]]),axis=0)
+            if (x>=b_ob1[0] and x<=b_ob1[2] and y>=b_ob1[1] and y<=b_ob1[3] or
+                x>=b_ob2p1[0] and x<=b_ob2p1[2] and y>=b_ob2p1[1] and y<=b_ob2p1[3] or
+                x>=b_ob2p2[0] and x<=b_ob2p2[2] and y>=b_ob2p2[1] and y<=b_ob2p2[3] or
+                x>=b_ob3[0] and x<=b_ob3[2] and y>=b_ob3[1] and y<=b_ob3[3]):
                 crash_grid[i][j] = 1
+
+                if (x>=ob1[0] and x<=ob1[2] and y>=ob1[1] and y<=ob1[3] or
+                    x>=ob2p1[0] and x<=ob2p1[2] and y>=ob2p1[1] and y<=ob2p1[3] or
+                    x>=ob2p2[0] and x<=ob2p2[2] and y>=ob2p2[1] and y<=ob2p2[3] or
+                    x>=ob3[0] and x<=ob3[2] and y>=ob3[1] and y<=ob3[3]):
+                    crash=np.append(crash,np.array([[180/np.pi*(lower_angle),180/np.pi*(upper_angle),x,y]]),axis=0)
+
             j += 1
         i += 1
-    return crash_grid, crash
-
+    return crash_grid,crash
 """
 crash=np.delete(crash,0,0)
 plt.plot(crash[:,0],crash[:,1],'.')
